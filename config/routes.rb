38591e691470bdfd418bed11db8b03ac11ad0a47
Armando Andini <armando.andini@hotmail.com>
2017-04-04 13:46:30 -0300

    Application routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root to: 'home#index'
 
-  get 'home/glosario'
-
-  get 'home/difusion'
-
-  get 'home/tos'
+  get 'glosario' => 'home#glosario'
+  get 'difusion' => 'home#difusion'
+  get 'tos' => 'home#tos'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
