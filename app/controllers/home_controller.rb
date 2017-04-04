6dabc0a00159973faa8b216c93df0003b7d5c0f2
Armando Andini <armando.andini@hotmail.com>
2017-04-04 11:51:00 -0300

    Generate home controller.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def glosario
+  end
+
+  def difusion
+  end
+
+  def tos
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/glosario'
+
+  get 'home/difusion'
+
+  get 'home/tos'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
