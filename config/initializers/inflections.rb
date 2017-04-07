f617996c1d4a2e3e029045de1e6addc1aafb3024
Armando Andini <armando.andini@hotmail.com>
2017-04-07 13:39:00 -0300

    Inflections for application models.

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -3,12 +3,15 @@
 # Add new inflection rules using the following format. Inflections
 # are locale specific, and you may define rules for as many different
 # locales as you wish. All of these examples are active by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.plural /^(ox)$/i, '\1en'
-#   inflect.singular /^(ox)en/i, '\1'
-#   inflect.irregular 'person', 'people'
-#   inflect.uncountable %w( fish sheep )
-# end
+#
+
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.irregular 'pais', 'paises'
+  inflect.irregular 'denuncia', 'denuncias'
+  inflect.irregular 'centro_de_denuncia', 'centro_de_denuncias'
+  inflect.irregular 'delito', 'delitos'
+  inflect.irregular 'legislacion', 'legislaciones'
+end
 
 # These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
