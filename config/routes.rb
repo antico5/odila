2b2293b7ec4332bf317d6100e8c84d97cf0dbad0
Armando Andini <armando.andini@hotmail.com>
2017-04-07 12:00:10 -0300

    Add 2015 and 2016 reports actions, views and assets.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
+  get 'reporte-2015' => 'home#reporte2015'
+  get 'reporte' => 'home#reporte'
   get 'glosario' => 'home#glosario'
   get 'difusion' => 'home#difusion'
   get 'tos' => 'home#tos'
