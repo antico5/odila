df249eef7f6c66e4e54eb93e828fbcfda3aadf7f
Armando Andini <armando.andini@hotmail.com>
2017-04-11 12:07:58 -0300

    Seeding script.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+puts 'Starting seeding'
+puts 'Reading db/seeds/ directory'
+files = Dir.glob 'db/seeds/*.yml'
+puts "Found #{ files.count } yml files."
+
+files.each do |seed_file|
+  puts "Processing #{ seed_file }"
+  data = YAML.load_file seed_file
+  data.each do |model_class, entity_list|
+    model_class = model_class.classify.constantize
+    puts "Before: #{ model_class.count } #{ model_class }"
+    entity_list.each { |entity_attrs| model_class.find_or_create_by entity_attrs }
+    puts "After: #{ model_class.count } #{ model_class }"
+  end
+end
