f6b4906e176f447d2b2a912568489545cb3037d2
Armando Andini <armando.andini@hotmail.com>
2017-05-02 11:49:47 -0300

    Seeds for preguntas.

--- app/models/pregunta.rb
+++ app/models/pregunta.rb
@@ -1,2 +1,7 @@
 class Pregunta < ApplicationRecord
+  enum tipo: { 'radio' => 1,
+            'checkbox' => 2,
+            'fecha' => 3,
+            'email' => 4,
+            'selectbox' => 5 }
 end
