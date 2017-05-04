07d81b26b18c57f482d1b61ee67aa1aa40c17f41
Armando Andini <armando.andini@hotmail.com>
2017-05-04 15:02:43 -0300

    Serialize opciones_multiples attribute.

--- app/models/item_denuncia.rb
+++ app/models/item_denuncia.rb
@@ -1,5 +1,7 @@
 class ItemDenuncia < ApplicationRecord
   belongs_to :pregunta
-  belongs_to :opcion
+  belongs_to :opcion, required: false
   belongs_to :denuncia, inverse_of: :item_denuncias
+
+  serialize :opciones_multiples, Array
 end
