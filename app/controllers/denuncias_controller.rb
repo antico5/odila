e6107f72ba05baa25a61ce6a6db5df320688dcb0
Armando Andini <armando.andini@hotmail.com>
2017-05-04 15:03:27 -0300

    Add correct redirect after denuncia and add additional strong params for items.

--- app/controllers/denuncias_controller.rb
+++ app/controllers/denuncias_controller.rb
@@ -2,17 +2,25 @@ class DenunciasController < ApplicationController
   def create
     @denuncia = Denuncia.new(denuncia_params)
 
-    respond_to do |format|
-      if @denuncia.save
-        redirect_to @denuncia, notice: 'Denuncia was successfully created.'
-      else
-        render :new
-      end
+    if @denuncia.save
+      redirect_to root_path, notice: 'Denuncia was successfully created.'
+    else
+      redirect_to root_path, notice: 'Denuncia was not created.'
     end
   end
 
   private
     def denuncia_params
-      params.require(:denuncia).permit(:pais_id, :delito_id, :fecha, item_denuncias_attributes: [:pregunta_id, :opcion_id])
+      params.require(:denuncia).permit(
+        :pais_id,
+        :delito_id,
+        item_denuncias_attributes: [
+          :pregunta_id,
+          :opcion_id,
+          :fecha,
+          :observacion,
+          opciones_multiples: []
+        ]
+      )
     end
 end
