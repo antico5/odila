1813963d6bac75bba3d4b903a499f59f8819e895
Armando Andini <armando.andini@hotmail.com>
2017-05-08 11:12:22 -0300

    Enable next button when option is picked.

--- app/assets/javascripts/denuncia.js
+++ app/assets/javascripts/denuncia.js
@@ -1,7 +1,8 @@
 // Muestra la pregunta con el data attribute 'numero' y esconde las demas.
 function mostrarPregunta(numero){
-  $('fieldset.pregunta').show()
   $('fieldset.pregunta:not([data-numero=' + numero + '])').hide()
+  $('fieldset.pregunta[data-numero=' + numero + ']').fadeIn(600)
+  location.hash = '#Denuncia'
 }
 
 $(document).ready(function(){
@@ -10,5 +11,13 @@ $(document).ready(function(){
 
   // Los botones con clase 'siguiente' llaman a mostrarPregunta con numero en el attribute
   // 'siguiente'
-  $('button.siguiente').on('click', function(){ mostrarPregunta($(this).data('siguiente')) })
+  $('button.siguiente').on('click', function(){
+    mostrarPregunta($(this).data('siguiente'))
+  })
+
+
+  // Los inputs con la clase 'opcion' deben habilitar el boton siguiente
+  $('.new_denuncia .opcion').on('change', function(){
+    $(this).closest('fieldset').find('button').removeAttr('disabled')
+  })
 })
