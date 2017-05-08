7bd8cf8f2556bbaf731b354e307bd853257ecf2e
Armando Andini <armando.andini@hotmail.com>
2017-05-08 12:49:13 -0300

    Fix html buttons.

--- app/helpers/denuncias_helper.rb
+++ app/helpers/denuncias_helper.rb
@@ -1,11 +1,11 @@
 module DenunciasHelper
   def boton_siguiente numero
-    button_tag 'Siguiente Pregunta', class: 'btn btn-warning siguiente',
+    button_tag 'Siguiente Pregunta', class: 'btn btn-warning siguiente', type: 'button',
       data: { siguiente: numero }, disabled: true
   end
 
   def boton_saltear numero
-    button_tag 'Saltear', class: 'btn btn-primary siguiente',
+    button_tag 'Saltear', class: 'btn btn-primary siguiente', type: 'button',
       data: { siguiente: numero }
   end
 end
