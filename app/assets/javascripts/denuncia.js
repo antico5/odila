17b6d4a7a5ce67abd8321d6514a515d4c32e5f7c
Armando Andini <armando.andini@hotmail.com>
2017-05-08 11:31:13 -0300

    Set form events only when denuncia form is present.

--- app/assets/javascripts/denuncia.js
+++ app/assets/javascripts/denuncia.js
@@ -1,3 +1,26 @@
+$(document).ready(function(){
+  setearEventosFormulario()
+})
+
+function setearEventosFormulario() {
+  if($('.new_denuncia').size() > 0){
+    // Mostrar la primera pregunta
+    mostrarPregunta(1)
+
+    // Los botones con clase 'siguiente' llaman a mostrarPregunta con numero en el attribute
+    // 'siguiente'
+    $('button.siguiente').on('click', function(){
+      mostrarPregunta($(this).data('siguiente'))
+    })
+
+
+    // Los inputs con la clase 'opcion' deben habilitar el boton siguiente
+    $('.new_denuncia .opcion').on('change', function(){
+      $(this).closest('fieldset').find('button').removeAttr('disabled')
+    })
+  }
+}
+
 // Muestra la pregunta con el data attribute 'numero' y esconde las demas.
 function mostrarPregunta(numero){
   $('fieldset.pregunta:not([data-numero=' + numero + '])').hide()
@@ -5,19 +28,3 @@ function mostrarPregunta(numero){
   location.hash = '#Denuncia'
 }
 
-$(document).ready(function(){
-  // Mostrar la primera pregunta
-  mostrarPregunta(1)
-
-  // Los botones con clase 'siguiente' llaman a mostrarPregunta con numero en el attribute
-  // 'siguiente'
-  $('button.siguiente').on('click', function(){
-    mostrarPregunta($(this).data('siguiente'))
-  })
-
-
-  // Los inputs con la clase 'opcion' deben habilitar el boton siguiente
-  $('.new_denuncia .opcion').on('change', function(){
-    $(this).closest('fieldset').find('button').removeAttr('disabled')
-  })
-})
