18b72415544126c98061217f9e84159a4b136505
Armando Andini <armando.andini@hotmail.com>
2017-05-16 10:07:25 -0300

    Add previous button to survey.

--- app/helpers/denuncias_helper.rb
+++ app/helpers/denuncias_helper.rb
@@ -1,11 +1,16 @@
 module DenunciasHelper
+  def boton_anterior numero
+    button_tag 'Anterior', class: 'btn btn-primary siguiente', type: 'button',
+      data: { siguiente: numero }
+  end
+
   def boton_siguiente numero
-    button_tag 'Siguiente Pregunta', class: 'btn btn-warning siguiente', type: 'button',
+    button_tag 'Siguiente', class: 'btn btn-warning siguiente', type: 'button',
       data: { siguiente: numero }, disabled: true
   end
 
   def boton_saltear numero
-    button_tag 'Saltear', class: 'btn btn-primary siguiente', type: 'button',
+    button_tag 'Saltear', class: 'btn btn-primary siguiente pull-right', type: 'button',
       data: { siguiente: numero }
   end
 end
