31a2de6412b8f02ec1445022dbf5199d92aa138a
Armando Andini <armando.andini@hotmail.com>
2017-05-23 12:36:19 -0300

    SMTP config for development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :domain => "gmail.com",
+    :port => 587,
+    :authentication => "plain",
+    :enable_starttls_auto => true,
+    :user_name => ENV['MAILER_USER'],
+    :password => ENV['MAILER_PASSWORD']
+
+  }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
